4364a24327cccab4d3a87895df2cf5a1445bea0a
Georges Labreche <georges.labreche@gmail.com>
2012-02-27 23:36:56 -0500

    Fixed minor issue with footer

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,16 @@
 
-<div id="theme_navigation_button">
-	<span id="theme_navigation_button_previous">&#8678</span>
+<div id="theme_navigation_button">	
+<!-- 
+	When we do not display the previous button, we use jQuery's hide() function on the following
+	theme_navigation_button_previous span element.
+	
+	This means that the display:none style is applied to the button span which eliminates the 
+	content of this div and shifts all of the following divs (theme_date_display container div 
+	and theme_navigation_button container div for the next theme button).
+	
+	In order to avoid this shift, we add a dummy invisible content: &nbsp;
+-->
+	<span>&nbsp;</span><span id="theme_navigation_button_previous">&#8678</span>
 </div>
 
 <div id="theme_date_display">
