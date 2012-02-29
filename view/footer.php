4ad9c78c022479ba2e20c519cef2918f19670de8
Georges Labreche <georges.labreche@gmail.com>
2012-02-28 21:35:59 -0500

    Added menu item in footer

--- view/footer.php
+++ view/footer.php
@@ -1,24 +1,35 @@
 
-<div id="theme_navigation_button">	
-<!-- 
-	When we do not display the previous button, we use jQuery's hide() function on the following
-	theme_navigation_button_previous span element.
+<div id="navigation">
+	<div id="theme_navigation_button">	
+	<!-- 
+		When we do not display the previous button, we use jQuery's hide() function on the following
+		theme_navigation_button_previous span element.
+		
+		This means that the display:none style is applied to the button span which eliminates the 
+		content of this div and shifts all of the following divs (theme_date_display container div 
+		and theme_navigation_button container div for the next theme button).
+		
+		In order to avoid this shift, we add a dummy invisible content: &nbsp;
+	-->
+		<span>&nbsp;</span><span id="theme_navigation_button_previous">&#8678</span>
+	</div>
 	
-	This means that the display:none style is applied to the button span which eliminates the 
-	content of this div and shifts all of the following divs (theme_date_display container div 
-	and theme_navigation_button container div for the next theme button).
+	<div id="theme_date_display">
+		<?php echo $theme_manager->getDisplayedThemeDate();?>
+	</div>
 	
-	In order to avoid this shift, we add a dummy invisible content: &nbsp;
--->
-	<span>&nbsp;</span><span id="theme_navigation_button_previous">&#8678</span>
+	
+	<div id="theme_navigation_button">
+		<span>&nbsp;</span><span id="theme_navigation_button_next">&#8680</span>
+	</div>
 </div>
 
-<div id="theme_date_display">
-	<?php echo $theme_manager->getDisplayedThemeDate();?>
+<div id="footer_links">
+	<a href="http://www.memerick.com">home</a> | 
+	<a href="http://memerick.com/urban-project/">urban project</a> | 
+	<a href="http://memerick.com/breakfast-project/">breakfast project</a> | 
+	<a href="http://www.emergencypress.com">emergency press</a>
 </div>
 
 
-<div id="theme_navigation_button">
-	<span id="theme_navigation_button_next">&#8680</span>
-</div>
 
